838ae45e0a7a82cb7086036d73d66cf1fb25479c
1aaron <aaronnek1@gmail.com>
2018-02-28 15:29:57 -0600

    refactored and created ViewControllers

---
+++ appSwoosh/appSwoosh/Controller/LeagueVC.swift
@@ -0,0 +1,18 @@
+//
+//  LeagueVC.swift
+//  appSwoosh
+//
+//  Created by Developer on 2/28/18.
+//  Copyright © 2018 Developer. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
