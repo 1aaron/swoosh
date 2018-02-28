237ad3700aef6b09ba3d44c70c3c44af9df95b0b
1aaron <aaronnek1@gmail.com>
2018-02-28 16:15:24 -0600

    added options function

--- appSwoosh/appSwoosh/Controller/LeagueVC.swift
+++ appSwoosh/appSwoosh/Controller/LeagueVC.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var btnNext: BorderButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "fromLeagueToSkill", sender: self)
+    }
+    
+    @IBAction func optionTapped(sender: UIButton) {
+        if let text = sender.currentTitle {
+            selectLeague(leagueType: text)
+        } else {
+            print("no title")
+        }
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeage = leagueType
+        btnNext.isEnabled = true
     }
 }
---
+++ appSwoosh/appSwoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  appSwoosh
+//
+//  Created by Developer on 2/28/18.
+//  Copyright © 2018 Developer. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeage: String?
+    var selectedSkillLvl: String?
+}
